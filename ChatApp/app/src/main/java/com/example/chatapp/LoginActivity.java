0f129905b16ba4a384da5a4d359ca19301a0fd94
anhquantr123 <57479794+anhquantr123@users.noreply.github.com>
2020-09-25 17:19:39 +0700

    du an android chat ap ngay 25/09/2020

---
+++ ChatApp/app/src/main/java/com/example/chatapp/LoginActivity.java
@@ -0,0 +1,98 @@
+package com.example.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView createNewAcc, tvQuenMatKhau;
+    private TextInputLayout email , password;
+    Button btndangnhap;
+    FirebaseAuth mAuth;
+    ProgressDialog mloasdingbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // ax
+        createNewAcc =  findViewById(R.id.createnewAccount);
+        tvQuenMatKhau = findViewById(R.id.forgotPassword);
+        email = findViewById(R.id.inputUserName);
+        btndangnhap = findViewById(R.id.dangnhap);
+        password = findViewById(R.id.textInputLayout);
+        mloasdingbar   = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        // end ax
+        createNewAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this ,CreateAccount.class);
+                startActivity(intent);
+            }
+        });
+
+        //
+        btndangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoiHamLogin();
+            }
+        });
+    }
+
+    private void GoiHamLogin() {
+        String Username  = email.getEditText().getText().toString().trim();
+        String Password = password.getEditText().getText().toString().trim();
+
+        // thuc hien test
+        if(Username.isEmpty() || Username.equals("")){
+            showErrow(email," Email Là Không Hợp Lệ!");
+        }else if (Password.isEmpty() || Password.length()<5){
+            showErrow(password, "PassWord trống hoặc nhỏ hơn 5 kí tự!");
+        }else{
+            mloasdingbar.setTitle("Đăng Nhập");
+            mloasdingbar.setMessage("Vui Lòng Chờ Giây Lát");
+            mloasdingbar.setCanceledOnTouchOutside(false);
+            mloasdingbar.show();
+            mAuth.signInWithEmailAndPassword(Username,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        mloasdingbar.dismiss();
+                        Toast.makeText(LoginActivity.this, "Đăng Nhập Thành Công", Toast.LENGTH_SHORT).show();
+                        Intent  intent = new Intent(LoginActivity.this,Setup.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        mloasdingbar.dismiss();
+                        Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+    }
+
+    private void showErrow(TextInputLayout loi, String text) {
+        loi.setError(text);
+        loi.requestFocus();
+    }
+
+
+}
